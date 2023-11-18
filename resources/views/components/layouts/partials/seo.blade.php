606c8b1b2d012998193196828678413beedee6c9
aripbudiman <muhamadarifbudiman22@gmail.com>
2023-11-18 21:57:59 +0700

    test

--- resources/views/components/layouts/partials/seo.blade.php
+++ resources/views/components/layouts/partials/seo.blade.php
@@ -2,20 +2,21 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <meta content="{{ csrf_token() }}" name="csrf-token">
-    <link href="{{ url()->current() }}" rel="canonical" />
+    {{-- <link href="{{ url()->current() }}" rel="canonical" />
     <meta content="{{ config('bacaan.sites.author') }}" name="author">
-    <meta content="{{ $description ?? config('bacaan.sites.description') }}" name="description">
+    <meta content="{{ $description ?? config('bacaan.sites.description') }}" name="description"> --}}
     {{-- <meta content="summary" name="twitter:card"> --}}
     {{-- <meta content="@kawankoding" name="twitter:site">
     <meta content="@kawankoding" name="twitter:creator"> --}}
-    <meta content="{{ $title ?? config('bacaan.sites.title') }} | {{ config('app.name') }}" name="twitter:title">
+    {{-- <meta content="{{ $title ?? config('bacaan.sites.title') }} | {{ config('app.name') }}" name="twitter:title">
+    --}}
     {{-- <meta content="https://bacaan.dev" name="twitter:url"> --}}
-    <meta content="{{ $description ?? config('bacaan.sites.description') }}" name="twitter:description">
+    {{-- <meta content="{{ $description ?? config('bacaan.sites.description') }}" name="twitter:description">
     <meta content="{{ $thumbnail ?? asset('assets/branding.jpg') }}" name="twitter:image:src">
     <meta content="{{ url()->current() }}" property="og:url">
-    <meta content="{{ $title ?? config('bacaan.sites.title') }} | {{ config('app.name') }}" property="og:title" />
-    <meta content="{{ $description ?? config('bacaan.sites.description') }}" property="og:description" />
-    <meta content="{{ $thumbnail ?? asset('assets/branding.jpg') }}" property="og:image">
+    <meta content="{{ $title ?? config('bacaan.sites.title') }} | {{ config('app.name') }}" property="og:title" /> --}}
+    {{-- <meta content="{{ $description ?? config('bacaan.sites.description') }}" property="og:description" /> --}}
+    {{-- <meta content="{{ $thumbnail ?? asset('assets/branding.jpg') }}" property="og:image"> --}}
 
     <link href="{{ asset('favicon.ico') }}" rel="shortcut icon" type="image/x-icon">
     <title>{{ $title ?? config('bacaan.sites.title') }} - {{ config('app.name', 'Bacaan Developer') }}</title>
